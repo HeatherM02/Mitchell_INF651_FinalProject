8d507bc2472c833bfd9c7f7e4e84f5af808c01c5
HeatherM02 <h_mitchell2@mail.fhsu.edu>
2023-12-11 16:55:27 -0600

    first commit final project

---
+++ script.js
@@ -0,0 +1,10 @@
+//Class for new prayer request entry
+class Request {
+  constructor(firstName, lastName, city, state, prayerRequest) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.city = city;
+    this.state = state;
+    this.prayerRequest = prayerRequest;
+  }
+}
